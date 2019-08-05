0e94bc8e0f81c3eef2aa35c04a9e8f3ed7716149
AbdullahElkolak <AbdullahElkolak@gmail.com>
2019-08-05 19:32:57 +0800

    update

--- server.js
+++ server.js
@@ -1,32 +1,134 @@
-const express = require('express');
-const app = express();
+const Joi = require('joi'); //moudle
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+
+
+mongoose.connect('mongodb://localhost/users', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log ('connected to database'))
 
 
 
-const courses = [
-  {id:1, name: 'course1'},
-  {id:2, name: 'course2'},
-  {id:3, name: 'course3'},
+app.use(express.json()); //method
+
+const users = 
+[
+  {
+    id:1, 
+    name: 'john Mike',
+    username:'Johnny',
+    email:'johnbanthall.com',
+    Adress:{
+      street:'11/6',
+      city:'NewYork',
+      zipcode:'12345'
+    }
+  },
+
+  {
+    id:2, 
+    name: 'Mike Hardy',
+    username:'Miky',
+    email:'Mikehardy.com',
+    Address:{
+      street:'35/29',
+      city:'Brooklyn',
+      zipcode:'12344'
+    }
+  },
+
+  {
+    id:3, 
+    name: 'Chris gary',
+    username:'gary',
+    email:'Chrisgary.com',
+    Address:{
+      street:'88/19',
+      city:'Los Angelos',
+      zipcode:'3344'
+    }
+  }
 ];
 
 app.get('/', (req, res) => {
-res.send('RESTful API');
+  res.send('RESTful dd');
 });
 
-app.get('/api/courses', (req, res) => {
-  res.send(...courses);
+app.get('/api/users/', (req, res) => {
+  res.send(...users);
 });
 
-app.get('/api/courses/:id', (req, res) => { 
-  const course = courses.find(c => c.id == parseInt(req.params.id));
-  if (!course) res.status(404).send('The course with the given ID was found');
-  res.send(course);
+app.get('/api/users/:id', (req, res) => { 
+  const user = users.find(c => c.id === parseInt(req.params.id));
+  if (!user) res.status(404).send('The users with the given ID was not found');
+  res.send(user);
 });
+
+app.post('/api/users', (req, res) => {
+  const {error} = validateUser(req.body); // getting result that error
+  if (error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const user = {
+    id: users.length + 1,
+    name: req.body.name,
+    username: req.body.username
+  };
+  users.push(user);
+  res.send(users);
+});
+
+app.put('/api/users/:id', (req, res) => {
+  //Look up thr user
+  //if not existing, return 404
+  const user = users.find(c => c.id === parseInt(req.params.id));
+  if (!user) res.status(404).send('The users with the given ID was found');
+  
+  //validate user
+  //If invalid, return 400 - bad req
+  const {error} = validateUser(req.body); // getting result that error
+  if (error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  
+  user.name = req.body.name;//update user
+  res.send(user); //return the update user
+});
+
+app.delete('/api/users/:id', (req, res) =>{
+  const user = users.find(c => c.id === parseInt(req.params.id));
+  if (!user) res.status(404).send('The users with the given ID was not found');
+  
+  const index = users.indexOf(user);
+  users.splice(index, 1);
+
+  res.send(user);
+
+});
+
+
+
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return result = Joi.validate(user, schema);
+
+}
+
+
 
 port = process.env.PORT || 4000;
-app.listen(port, function () {
+  app.listen(port, function () {
  console.log('RESTful API started on'+port)
-})
+});
 
  
 
